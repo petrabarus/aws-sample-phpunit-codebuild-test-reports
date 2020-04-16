a27f2ed6676824814687ac2f8a8c3b1f3d4322fc
Petra Barus <petra.barus@gmail.com>
2020-04-16 07:22:20 +0700

    add testcase message.

--- tests/MyClassTest.php
+++ tests/MyClassTest.php
@@ -6,13 +6,13 @@ class MyClassTest extends TestCase {
 
     public function testMethod1() {
         $object = new MyClass();
-        $this->assertEquals(2, $object->method1(1, 1));
-        $this->assertEquals(3, $object->method1(1, 2));
+        $this->assertEquals(2, $object->method1(1, 1), 'Test Method 1 Case 1');
+        $this->assertEquals(3, $object->method1(1, 2), 'Test Method 1 Case 2');
     }
 
     public function testMethod2() {
         $object = new MyClass();
-        $this->assertEquals('11', $object->method2('1', '1'));
-        $this->assertEquals('123', $object->method2('1', '2'));
+        $this->assertEquals('11', $object->method2('1', '1'), 'Test Method 2 Case 1');
+        $this->assertEquals('123', $object->method2('1', '2'), 'Test Method 2 Case 2');
     }
 }
